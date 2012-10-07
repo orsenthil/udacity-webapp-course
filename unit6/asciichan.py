6d7e108e8cbbe9788e5cb6b43272ffa9ecfe9a1a
Senthil Kumaran <senthil@uthcode.com>
2012-10-06 17:40:26 -0700

    memache using dict

---
+++ unit6/asciichan.py
@@ -0,0 +1,106 @@
+import os
+import webapp2
+import jinja2
+import urllib2
+import logging
+from xml.dom import minidom
+
+from google.appengine.ext import db
+from google.appengine.api import memcache
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    coords = db.GeoPtProperty()
+
+IP_URL = "http://api.hostip.info/?ip="
+
+CACHE = {}
+
+def top_arts(update = False):
+    key = 'top'
+
+    if not update and key in CACHE:
+        arts = CACHE[key]
+    else:
+        logging.error("DB QUERY")
+        arts = db.GqlQuery("""SELECT * FROM Art ORDER BY created DESC LIMIT 10""")
+        arts = list(arts)
+        CACHE[key] = arts
+
+    return arts
+
+
+def get_coords(ip):
+    # ip = "4.2.2.2" # Test URL
+    url = IP_URL + ip
+    content = None
+    try:
+        content = urllib2.urlopen(url).read()
+    except urllib2.URLError:
+        return
+    if content:
+        try:
+            d = minidom.parseString(content)
+            elem = d.getElementsByTagName("gml:coordinates")[0]
+            coords = elem.childNodes[0]
+            lon, lat = coords.nodeValue.split(',')
+            return db.GeoPt(lat, lon)
+        except IndexError:
+            return None
+
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+
+def gmaps_img(points):
+    markers = []
+    for p in points:
+        coords = 'markers=%d,%d' % (p.lat, p.lon)
+        markers.append(coords)
+    return GMAPS_URL + '&'.join(markers)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kws):
+        self.response.out.write(*args, **kws)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self, template, **kws):
+        self.write(self.render_str(template, **kws))
+
+class MainPage(Handler):
+
+    def render_front(self, title="", art="", error=""):
+        arts = top_arts()
+        points = []
+        for a in arts:
+            if a.coords:
+                points.append(a.coords)
+        img_url = gmaps_img(points)
+        self.render("front.html", title=title, art=art, error=error, arts=arts, img_url=img_url)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            coords = get_coords(self.request.remote_addr)
+            a = Art(title=title, art=art)
+            if coords:
+                a.coords = coords
+            a.put()
+            self.redirect("/")
+        else:
+            error = "We need both title and art"
+            self.render_front(title=title, art=art, error=error)
+
+        top_arts(True)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
---
+++ unit6/memcached.py
@@ -0,0 +1,35 @@
+# QUIZ implement the basic memcache functions
+
+CACHE = {}
+
+#return True after setting the data
+def set(key, value):
+    CACHE[key] = value
+    return True
+
+#return the value for key
+def get(key):
+    if key in CACHE:
+        return CACHE[key]
+
+#delete key from the cache
+def delete(key):
+    if key in CACHE:
+        del CACHE[key]
+
+#clear the entire cache
+def flush():
+    CACHE.clear()
+
+print set('x', 1)
+#>>> True
+
+print get('x')
+#>>> 1
+
+print get('y')
+#>>> None
+
+delete('x')
+print get('x')
+#>>> None
